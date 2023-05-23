f6d940bd29efefad056370be32b868a75562acf1
RokwonK <wonrok97@naver.com>
2023-05-23 21:51:45 +0900

    ✨ Color Style 추가

---
+++ lib/src/util/color/color_style.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static Color get mainColor => const Color(0xFF1F5CD1);
+
+  // black
+  static Color get black100 => const Color(0xFF7E7E88);
+  static Color get black200 => const Color(0xFF6D6D75);
+  static Color get black300 => const Color(0xFF606169);
+  static Color get black400 => const Color(0xFF505057);
+  static Color get black500 => const Color(0xFF494A53);
+  static Color get black600 => const Color(0xFF3E404A);
+  static Color get black700 => const Color(0xFF33343E);
+  static Color get black800 => const Color(0xFF282933);
+  static Color get black900 => const Color(0xFF1C1D28);
+  static Color get black1000 => Colors.black;
+
+  // white
+  static Color get white => Colors.white;
+  static Color get white100 => const Color(0xFFF7F8F9);
+  static Color get white200 => const Color(0xFFF2F4F6);
+  static Color get white300 => const Color(0xFFEDEFF0);
+  static Color get white400 => const Color(0xFFE9ECEE);
+  static Color get white500 => const Color(0xFFE3E6E9);
+  static Color get white600 => const Color(0xFFD9DEE0);
+  static Color get white700 => const Color(0xFFD0D5D8);
+  static Color get white800 => const Color(0xFFC5CCD0);
+  static Color get white900 => const Color(0xFFB8C0C5);
+  static Color get white1000 => const Color(0xFFABB3B8);
+
+  // blue
+  static Color get blue100 => const Color(0xFFB2CDFF);
+  static Color get blue200 => const Color(0xFF98BBFF);
+  static Color get blue300 => const Color(0xFF88B0FF);
+  static Color get blue400 => const Color(0xFF70A0FF);
+  static Color get blue500 => const Color(0xFF5E8BFF);
+  static Color get blue600 => const Color(0xFF4D7EFD);
+  static Color get blue700 => const Color(0xFF4770FF);
+  static Color get blue800 => const Color(0xFF3864FF);
+  static Color get blue900 => const Color(0xFF2653F3);
+  static Color get blue1000 => const Color(0xFF1F5CD1);
+}
