dfd1914f3f97fde64e39bf6eaa98c7201300a74c
RokwonK <wonrok97@naver.com>
2023-05-27 15:19:32 +0900

    ✨ 통신결과 State 추가

---
+++ lib/src/util/resources/data_state.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+abstract class DataState<T> {
+  final T? data;
+  final DioError? error;
+
+  bool get isSuccess => data != null;
+
+  const DataState({this.data, this.error});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  const DataFailed(DioError error) : super(error: error);
+}
