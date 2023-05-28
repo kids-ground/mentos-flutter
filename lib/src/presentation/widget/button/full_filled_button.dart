71eb320ba7552d66713b50eaeb823014742e28d5
RokwonK <wonrok97@naver.com>
2023-05-29 01:11:57 +0900

    ✨ Full Filled Button 추가

---
+++ lib/src/presentation/widget/button/full_filled_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mentos_flutter/src/util/color/color_style.dart';
+
+class FullFilledButton extends StatelessWidget {
+  const FullFilledButton({
+    Key? key,
+    required this.title,
+    this.fontSize = 18,
+    this.backgroundColor = const Color(0xFF1F5CD1),
+    this.fontColor = const Color(0xFFFFFFFF),
+    this.width = double.infinity,
+    this.height = double.infinity,
+    required this.onPressed
+  }) : super(key: key);
+
+  final String title;
+  final double fontSize;
+  final Color backgroundColor;
+  final Color fontColor;
+  final double width;
+  final double height;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      padding: const EdgeInsets.all(0),
+      onPressed: onPressed,
+      child: Container(
+        alignment: Alignment.center,
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: backgroundColor,
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.w500,
+            color: fontColor,
+          )
+        ),
+      ),
+    );
+  }
+}
