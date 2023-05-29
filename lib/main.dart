593d0e06fa357d4b8b8b205054835c1ba1ec2d82
RokwonK <wonrok97@naver.com>
2023-05-30 00:25:19 +0900

    🎨 main, app 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:mentos_flutter/src/presentation/page/app/bloc/app_bloc.dart';
+import 'package:mentos_flutter/src/presentation/page/app/view/app_view.dart';
 import 'package:mentos_flutter/src/presentation/page/login/bloc/login_bloc.dart';
 import 'package:mentos_flutter/src/presentation/page/login/view/login.dart';
+import 'package:mentos_flutter/src/presentation/page/main_tab/bloc/main_tab_bloc.dart';
 import 'package:mentos_flutter/src/presentation/page/terms_of_service/bloc/terms_of_service_bloc.dart';
 import 'package:mentos_flutter/src/util/color/color_style.dart';
 import 'package:mentos_flutter/src/util/constant/strings.dart';
 
 void main() {
+  // 여기서 member 확인??
+  // 확인하고 MyApp에 AppStatus 넘겨주기 joined or unAuth
   KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
   runApp(const MyApp());
 }
@@ -15,23 +20,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider(create: (context) => AppBloc()),
         BlocProvider(create: (context) => LoginBloc()),
         BlocProvider(create: (context) => TermsOfServiceBloc()),
+        BlocProvider(create: (context) => MainTabBloc())
       ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: ColorStyles.black100),
-          fontFamily: 'Pretendard',
-          useMaterial3: true,
-        ),
-        home: const LonginPage(),
-      ),
+      child: AppView()
     );
   }
 }
