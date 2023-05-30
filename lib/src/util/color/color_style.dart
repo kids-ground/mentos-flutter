384aa3f13df19d1459af03aaff7eb7936011a138
RokwonK <wonrok97@naver.com>
2023-05-30 21:07:30 +0900

    ✨ color red 계열 추가

--- lib/src/util/color/color_style.dart
+++ lib/src/util/color/color_style.dart
@@ -2,42 +2,54 @@
 import 'package:flutter/material.dart';
 
 class ColorStyles {
-  static Color get mainColor => const Color(0xFF1F5CD1);
+  static const Color mainColor = Color(0xFF1F5CD1);
 
   // black
-  static Color get black100 => const Color(0xFF7E7E88);
-  static Color get black200 => const Color(0xFF6D6D75);
-  static Color get black300 => const Color(0xFF606169);
-  static Color get black400 => const Color(0xFF505057);
-  static Color get black500 => const Color(0xFF494A53);
-  static Color get black600 => const Color(0xFF3E404A);
-  static Color get black700 => const Color(0xFF33343E);
-  static Color get black800 => const Color(0xFF282933);
-  static Color get black900 => const Color(0xFF1C1D28);
-  static Color get black1000 => Colors.black;
+  static const Color black100 = Color(0xFF7E7E88);
+  static const Color black200 = Color(0xFF6D6D75);
+  static const Color black300 = Color(0xFF606169);
+  static const Color black400 = Color(0xFF505057);
+  static const Color black500 = Color(0xFF494A53);
+  static const Color black600 = Color(0xFF3E404A);
+  static const Color black700 = Color(0xFF33343E);
+  static const Color black800 = Color(0xFF282933);
+  static const Color black900 = Color(0xFF1C1D28);
+  static const Color black1000 = Colors.black;
 
   // white
-  static Color get white => Colors.white;
-  static Color get white100 => const Color(0xFFF7F8F9);
-  static Color get white200 => const Color(0xFFF2F4F6);
-  static Color get white300 => const Color(0xFFEDEFF0);
-  static Color get white400 => const Color(0xFFE9ECEE);
-  static Color get white500 => const Color(0xFFE3E6E9);
-  static Color get white600 => const Color(0xFFD9DEE0);
-  static Color get white700 => const Color(0xFFD0D5D8);
-  static Color get white800 => const Color(0xFFC5CCD0);
-  static Color get white900 => const Color(0xFFB8C0C5);
-  static Color get white1000 => const Color(0xFFABB3B8);
+  static const Color white = Colors.white;
+  static const Color white100 = Color(0xFFF7F8F9);
+  static const Color white200 = Color(0xFFF2F4F6);
+  static const Color white300 = Color(0xFFEDEFF0);
+  static const Color white400 = Color(0xFFE9ECEE);
+  static const Color white500 = Color(0xFFE3E6E9);
+  static const Color white600 = Color(0xFFD9DEE0);
+  static const Color white700 = Color(0xFFD0D5D8);
+  static const Color white800 = Color(0xFFC5CCD0);
+  static const Color white900 = Color(0xFFB8C0C5);
+  static const Color white1000 = Color(0xFFABB3B8);
 
   // blue
-  static Color get blue100 => const Color(0xFFB2CDFF);
-  static Color get blue200 => const Color(0xFF98BBFF);
-  static Color get blue300 => const Color(0xFF88B0FF);
-  static Color get blue400 => const Color(0xFF70A0FF);
-  static Color get blue500 => const Color(0xFF5E8BFF);
-  static Color get blue600 => const Color(0xFF4D7EFD);
-  static Color get blue700 => const Color(0xFF4770FF);
-  static Color get blue800 => const Color(0xFF3864FF);
-  static Color get blue900 => const Color(0xFF2653F3);
-  static Color get blue1000 => const Color(0xFF1F5CD1);
+  static const Color blue100 = Color(0xFFE9EFFB);
+  static const Color blue200 = Color(0xFFD2DEF6);
+  static const Color blue300 = Color(0xFFBBCEF1);
+  static const Color blue400 = Color(0xFFA5BEED);
+  static const Color blue500 = Color(0xFF8FADE8);
+  static const Color blue600 = Color(0xFF799DE3);
+  static const Color blue700 = Color(0xFF628CDE);
+  static const Color blue800 = Color(0xFF4C7DDA);
+  static const Color blue900 = Color(0xFF366DD6);
+  static const Color blue1000 = Color(0xFF1F5CD1);
+
+  // red
+  static const Color red100 = Color(0xFFFEE7EB);
+  static const Color red200 = Color(0xFFFCCED6);
+  static const Color red300 = Color(0xFFFAB5C1);
+  static const Color red400 = Color(0xFFF99DAE);
+  static const Color red500 = Color(0xFFF88499);
+  static const Color red600 = Color(0xFFF76C85);
+  static const Color red700 = Color(0xFFF55370);
+  static const Color red800 = Color(0xFFF43B5C);
+  static const Color red900 = Color(0xFFF32348);
+  static const Color red1000 = Color(0xFFF10A34);
 }
