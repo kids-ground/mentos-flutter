81c45dace2d401594b7beac63d53d90a2ef51009
RokwonK <wonrok97@naver.com>
2023-05-30 21:08:31 +0900

    ✨ main tabbar 스타일 적용

--- lib/src/presentation/page/main_tab/view/main_tab.dart
+++ lib/src/presentation/page/main_tab/view/main_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentos_flutter/src/presentation/page/chat_list/view/chat_list.dart';
+import 'package:mentos_flutter/src/presentation/page/home/bloc/home_bloc.dart';
 import 'package:mentos_flutter/src/presentation/page/home/view/home.dart';
 import 'package:mentos_flutter/src/presentation/page/main_tab/bloc/main_tab_bloc.dart';
 import 'package:mentos_flutter/src/presentation/page/mentor_list/view/mentor_list.dart';
@@ -37,54 +38,59 @@ class MainTabPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final mainTabBloc = context.read<MainTabBloc>();
 
-    return BlocBuilder<MainTabBloc, MainTabState>(
-      builder: (context, state) => WillPopScope(
-        onWillPop: null,
-        child: Scaffold(
-          body: pageList.elementAt(state.selectedIndex),
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: true,
-            showUnselectedLabels: true,
-            elevation: 0.1,
-            selectedItemColor: ColorStyles.black700,
-            unselectedItemColor: ColorStyles.white800,
-            selectedLabelStyle: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w600
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => HomeBloc())
+      ],
+      child: BlocBuilder<MainTabBloc, MainTabState>(
+        builder: (context, state) => WillPopScope(
+          onWillPop: null,
+          child: Scaffold(
+            body: pageList.elementAt(state.selectedIndex),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: true,
+              showUnselectedLabels: true,
+              elevation: 0.1,
+              selectedItemColor: ColorStyles.black700,
+              unselectedItemColor: ColorStyles.white800,
+              selectedLabelStyle: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w600
+              ),
+              unselectedLabelStyle: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w600
             ),
-            unselectedLabelStyle: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w600
-          ),
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: getItemImage(index: 0, isSelected: false),
-                activeIcon: getItemImage(index: 0, isSelected: true),
-                label: '홈',
-              ),
-              BottomNavigationBarItem(
-                icon: getItemImage(index: 1, isSelected: false),
-                activeIcon: getItemImage(index: 1, isSelected: true),
-                label: '멘토',
-              ),
-              BottomNavigationBarItem(
-                icon: getItemImage(index: 2, isSelected: false),
-                activeIcon: getItemImage(index: 2, isSelected: true),
-                label: '채팅',
-              ),
-              BottomNavigationBarItem(
-                icon: getItemImage(index: 3, isSelected: false),
-                activeIcon: getItemImage(index: 3, isSelected: true),
-                label: '설정',
-              ),
-            ],
-            currentIndex: state.selectedIndex, // 지정 인덱스로 이동
-            onTap: (idx) {
-              mainTabBloc.add(MainTabSelectedItem(selectedIndex: idx));
-            }, // 선언했던 onItemTapped,
-          ),
-        )),
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: getItemImage(index: 0, isSelected: false),
+                  activeIcon: getItemImage(index: 0, isSelected: true),
+                  label: '홈',
+                ),
+                BottomNavigationBarItem(
+                  icon: getItemImage(index: 1, isSelected: false),
+                  activeIcon: getItemImage(index: 1, isSelected: true),
+                  label: '멘토',
+                ),
+                BottomNavigationBarItem(
+                  icon: getItemImage(index: 2, isSelected: false),
+                  activeIcon: getItemImage(index: 2, isSelected: true),
+                  label: '채팅',
+                ),
+                BottomNavigationBarItem(
+                  icon: getItemImage(index: 3, isSelected: false),
+                  activeIcon: getItemImage(index: 3, isSelected: true),
+                  label: '설정',
+                ),
+              ],
+              currentIndex: state.selectedIndex, // 지정 인덱스로 이동
+              onTap: (idx) {
+                mainTabBloc.add(MainTabSelectedItem(selectedIndex: idx));
+              }, // 선언했던 onItemTapped,
+            ),
+          )),
+      ),
     );
   }
 }
