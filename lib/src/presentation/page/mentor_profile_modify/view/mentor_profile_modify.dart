e5c3934e736b978e3967236b9d64c793c179f14a
RokwonK <wonrok97@naver.com>
2023-09-25 20:30:24 +0900

    ✨ 멘토 프로필 작성 페이지 추가

---
+++ lib/src/presentation/page/mentor_profile_modify/view/mentor_profile_modify.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mentos_flutter/src/presentation/widget/app_bar/app_bar.dart';
+import 'package:mentos_flutter/src/util/color/color_style.dart';
+
+class ProfileModifyPage extends StatelessWidget {
+  const ProfileModifyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CommonAppBar(title: '멘토 프로필',),
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          color: ColorStyles.white200,
+          child: Text(
+            '멘토 프로필 수정 페이지',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: ColorStyles.black100
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
