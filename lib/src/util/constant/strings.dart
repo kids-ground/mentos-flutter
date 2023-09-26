6a91971719097b529a52584298075ca68f88c491
RokwonK <wonrok97@naver.com>
2023-09-26 17:20:37 +0900

    ✨ jobGroup 리스트 추가

--- lib/src/util/constant/strings.dart
+++ lib/src/util/constant/strings.dart
@@ -6,3 +6,6 @@ const String kakaoNativeAppKey = 'bfe7b97ac3e5b3b6d9d1cd55da609ae8';
 const String baseApi = 'https://rokwonk.com';
 
 const String fontFamily = '';
+
+
+const List<String> jobGroupList = <String>['개발', '디자인', '마케팅', '인사'];
