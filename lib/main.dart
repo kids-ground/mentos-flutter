b5ad4cea3fb5c7ede759ee18cfdaae3593a3523b
RokwonK <wonrok97@naver.com>
2023-09-30 20:36:47 +0900

    🎨 main 및 app 역할 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import 'package:mentos_flutter/src/presentation/page/app/bloc/app_bloc.dart';
-import 'package:mentos_flutter/src/presentation/page/app/view/app_view.dart';
-import 'package:mentos_flutter/src/presentation/page/login/bloc/login_bloc.dart';
-import 'package:mentos_flutter/src/presentation/page/login/view/login.dart';
-import 'package:mentos_flutter/src/presentation/page/main_tab/bloc/main_tab_bloc.dart';
-import 'package:mentos_flutter/src/presentation/page/terms_of_service/bloc/terms_of_service_bloc.dart';
-import 'package:mentos_flutter/src/util/color/color_style.dart';
+import 'package:mentos_flutter/src/presentation/page/app/view/app.dart';
 import 'package:mentos_flutter/src/util/constant/strings.dart';
 
 void main() {
-  // 여기서 member 확인??
-  // 확인하고 MyApp에 AppStatus 넘겨주기 joined or unAuth
+  // 기반 설정
   KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AppBloc()),
-        BlocProvider(create: (context) => LoginBloc()),
-        BlocProvider(create: (context) => TermsOfServiceBloc()),
-        BlocProvider(create: (context) => MainTabBloc())
-      ],
-      child: AppView()
-    );
-  }
+  runApp(const App());
 }
--- lib/src/presentation/page/app/view/app.dart
+++ lib/src/presentation/page/app/view/app.dart
@@ -2,9 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentos_flutter/src/config/router/app_router.dart';
 import 'package:mentos_flutter/src/presentation/page/app/bloc/app_bloc.dart';
+import 'package:mentos_flutter/src/presentation/page/login/bloc/login_bloc.dart';
 import 'package:mentos_flutter/src/presentation/page/login/view/login.dart';
+import 'package:mentos_flutter/src/presentation/page/main_tab/bloc/main_tab_bloc.dart';
 import 'package:mentos_flutter/src/presentation/page/main_tab/view/main_tab.dart';
+import 'package:mentos_flutter/src/presentation/page/terms_of_service/bloc/terms_of_service_bloc.dart';
 import 'package:mentos_flutter/src/util/color/color_style.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => AppBloc()),
+          BlocProvider(create: (context) => LoginBloc()),
+          BlocProvider(create: (context) => TermsOfServiceBloc()),
+          BlocProvider(create: (context) => MainTabBloc())
+        ],
+        child: const AppView()
+    );
+  }
+}
+
 
 class AppView extends StatelessWidget {
   const AppView({Key? key}) : super(key: key);
