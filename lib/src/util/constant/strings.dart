1ebe60ebb8adede2c8bd187aa61c72cb97fac93a
RokwonK <wonrok97@naver.com>
2023-09-30 17:56:56 +0900

    ✨ email, link Pattern String 추가

--- lib/src/util/constant/strings.dart
+++ lib/src/util/constant/strings.dart
@@ -5,7 +5,9 @@ const String kakaoNativeAppKey = 'bfe7b97ac3e5b3b6d9d1cd55da609ae8';
 // API
 const String baseApi = 'https://rokwonk.com';
 
-const String fontFamily = '';
+// Pattern
+const emailPattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+const kakaoOpenChatLinkPattern = r'^https:\/\/open\.kakao\.com\/[^/]+\/[^/]+$';
 
 
 const List<String> jobGroupList = <String>['개발', '디자인', '마케팅', '인사'];
