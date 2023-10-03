02d8925a1494d77ae49c3dfe8571844f62070ac1
RokwonK <wonrok97@naver.com>
2023-10-03 19:03:52 +0900

    ✨ top snackbar 추가

---
+++ lib/src/presentation/widget/dialog/loading_dialog.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mentos_flutter/src/util/color/color_style.dart';
+
+void showLoadingDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return const Dialog(
+          backgroundColor: Colors.transparent,
+          surfaceTintColor: Colors.transparent,
+          child: CupertinoActivityIndicator(
+            radius: 20,
+            color: ColorStyles.white,
+          )
+      );
+    },
+  );
+}
+
+void showSimpleSnackBar(BuildContext context, String text) {
+  SnackBar snackBar = SnackBar(
+    content: Text(text),
+    behavior: SnackBarBehavior.floating,
+    dismissDirection: DismissDirection.up,
+    duration: const Duration(milliseconds: 2000),
+    margin: EdgeInsets.only(
+        bottom: MediaQuery.of(context).size.height - 200,
+        left: 10,
+        right: 10),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
