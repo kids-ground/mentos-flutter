61451b201221c26652b675c45b53c5b8c26dda72
RokwonK <wonrok97@naver.com>
2023-10-04 14:30:00 +0900

    ✨ 멘토 상세 패이지 뷰 완성

--- lib/src/presentation/page/mentor_detail/view/mentor_detail_page.dart
+++ lib/src/presentation/page/mentor_detail/view/mentor_detail_page.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -43,14 +44,14 @@ class _MentorDetailView extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _ProfileImage(),
-                    const SizedBox(height: 24,),
+                    _ProfileView(),
+                    const SizedBox(height: 32,),
                     _StatisticsView(),
-                    const SizedBox(height: 24,),
+                    const SizedBox(height: 32,),
                     _IntroductionView(),
-                    const SizedBox(height: 24,),
+                    const SizedBox(height: 32,),
                     _DescriptionView(),
-                    const SizedBox(height: 24,),
+                    const SizedBox(height: 16,),
                   ],
                 )
               ),
@@ -63,12 +64,106 @@ class _MentorDetailView extends StatelessWidget {
   }
 }
 
-class _ProfileImage extends StatelessWidget {
-  const _ProfileImage({Key? key}) : super(key: key);
+class _ProfileView extends StatelessWidget {
+  const _ProfileView({Key? key}) : super(key: key);
+
+  final double width = 64;
+  final double height = 64;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        CachedNetworkImage(
+          width: width,
+          height: height,
+          imageUrl: "https://images.velog.io/images/chang626/post/c9533c4f-adbb-4411-bce4-b09293d64fbf/A03EACB4-4DFA-439A-A3FE-084635A89FE6.png",
+          imageBuilder: (context, imageProvider) => Container(
+            decoration: BoxDecoration(
+              color: ColorStyles.blue300,
+              borderRadius: BorderRadius.all(Radius.circular(width/2)),
+              image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                  scale: 0.5
+              ),
+            ),
+          ),
+          placeholder: (context, url) => Container(width: width, height: height,),
+        ),
+        const SizedBox(width: 16,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "로건",
+              style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  color: ColorStyles.black1000
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  "Google",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: ColorStyles.black800
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(4,0,0,0),
+                  // 인증 상태에 따라 인증 진행중일 수도 있음
+                  child: Image.asset(
+                    'assets/images/certification_mark.png',
+                    width: 16,
+                    height: 16,
+                    color: Colors.green[300],
+                  ),
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Text(
+                  "연구개발",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: ColorStyles.black100
+                  ),
+                ),
+                Text(' ∙ '),
+                Text(
+                  "AI",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: ColorStyles.black100
+                  ),
+                ),
+                Text('  '),
+                Text(
+                  "(3 ~ 4년차)",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: ColorStyles.black100
+                  ),
+                ),
+              ],
+            ),
+
+
+          ],
+        ),
+
+
+      ],
+    );
   }
 }
 
@@ -77,7 +172,114 @@ class _StatisticsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                decoration: BoxDecoration(
+                    color: ColorStyles.white,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: ColorStyles.blue500)
+                ),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/palette.png', width: 26, color: ColorStyles.blue300,),
+                    const SizedBox(height: 4,),
+                    Text(
+                      "멘토링",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: ColorStyles.black800
+                      ),
+                    ),
+                    const SizedBox(height: 4,),
+                    Text(
+                      "5번",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: ColorStyles.black800
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ),
+          const SizedBox(width: 16,),
+          Expanded(
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                decoration: BoxDecoration(
+                    color: ColorStyles.white,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: ColorStyles.blue500)
+                ),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/letter.png', width: 26, color: ColorStyles.blue300,),
+                    const SizedBox(height: 4,),
+                    Text(
+                      "리뷰",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: ColorStyles.black800
+                      ),
+                    ),
+                    const SizedBox(height: 4,),
+                    Text(
+                      "12개",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: ColorStyles.black800
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ),
+          const SizedBox(width: 16,),
+          Expanded(
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                decoration: BoxDecoration(
+                    color: ColorStyles.white,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: ColorStyles.blue500)
+                ),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/star.png', width: 26, color: ColorStyles.blue300,),
+                    const SizedBox(height: 4,),
+                    Text(
+                      "평점",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: ColorStyles.black800
+                      ),
+                    ),
+                    const SizedBox(height: 4,),
+                    Text(
+                      "4.5",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: ColorStyles.black800
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -86,7 +288,35 @@ class _IntroductionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '한 줄 인사',
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: ColorStyles.black800
+          ),
+        ),
+        const SizedBox(height: 12),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          decoration: BoxDecoration(
+            color: ColorStyles.blue100,
+            borderRadius: BorderRadius.circular(8)
+          ),
+          child: Text(
+            '안녕하세요. Google 5년차 소프트웨어 엔지니어입니다. Google에 대해 궁금하신게 있으시다면 주저말고 연락주세요!',
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: ColorStyles.black1000
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
@@ -95,7 +325,46 @@ class _DescriptionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '소개',
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: ColorStyles.black800
+          ),
+        ),
+        const SizedBox(height: 12),
+
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          decoration: BoxDecoration(
+              color: ColorStyles.white200,
+              borderRadius: BorderRadius.circular(8)
+          ),
+          child: Text('''
+간단하게 제 소개를 해볼게요ㅎㅎ
+
+- 삼성 사내 벤쳐 스타트업 '앱플' 대표
+- '애플'에 매각 
+- 아이폰 15 디자인 리드
+- 아이폰 16 디자인 리드 어쩌면?
+- 구글 코리아 L4 엔지니어
+- 구글 코리아 본부장 각
+
+제 프로필을 보시고 궁금하신 점이 있으시다면 언제든 연락주세요  
+            ''',
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: ColorStyles.black800
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
@@ -153,7 +422,6 @@ class _ContactView extends StatelessWidget {
                     color: state.isBookmarked ? ColorStyles.yellow : ColorStyles.white500,
                   )
                 ),
-                // child: Image.asset('assets/images/star.png', width: 24, color: ColorStyles.white600,)
                 child: Image.asset(
                   'assets/images/star.png',
                   width: 24,
