2a1c5388c27538142529c544ef1beb47232b109b
RokwonK <wonrok97@naver.com>
2023-10-04 16:14:41 +0900

    🎨 Mentor Profile Response 내 새 값 추가

--- lib/src/domain/dto/response/mentor_profile/mentor_profile_response.dart
+++ lib/src/domain/dto/response/mentor_profile/mentor_profile_response.dart
@@ -7,20 +7,33 @@ class MentorProfileResponse extends Equatable {
     this.email,
     this.verifyType,
     this.openTalkLink,
+    this.jobYear,
     this.jobGroup,
     this.detailJob,
     this.introduction,
-    this.description
+    this.description,
+    this.mentoringCount,
+    this.reviewCount,
+    this.reviewRate
   );
 
+  // 개인용
   String email;
   MentorVerifyType verifyType;
+
+  // 공통
   String openTalkLink;
   JobGroup jobGroup;
+  JobYear jobYear;
   String detailJob;
   String introduction;
   String description;
 
+  // 공개용
+  int mentoringCount;
+  int reviewCount;
+  double reviewRate;
+
   @override
-  List<Object?> get props => [email, verifyType, openTalkLink, jobGroup, detailJob, introduction, description];
+  List<Object?> get props => [email, verifyType, openTalkLink, jobYear, jobGroup, detailJob, introduction, description, mentoringCount, reviewCount, reviewRate];
 }
