283e90f3d924dc94ceb94ee9b8e578e8bbd13907
RokwonK <wonrok97@naver.com>
2023-10-24 17:24:33 +0900

    ✨ retrofit, dio 셋팅 코드추가

--- lib/src/config/retrofit_config.dart
+++ lib/src/config/retrofit_config.dart
@@ -1,5 +1,49 @@
 
 
-void setupRetrofitConfig() {
+import 'package:dio/dio.dart';
+import 'package:mentos_flutter/src/data/repository/auth_repository.dart';
+import 'package:mentos_flutter/src/util/constant/strings.dart';
+import 'package:mentos_flutter/src/util/resource/logger.dart';
 
+late final Dio baseApiDio;
+
+void setupRetrofitConfig() {
+  _setupBaseApiDio();
+}
+
+void _setupBaseApiDio() {
+  baseApiDio = Dio();
+
+  // timeout 설정
+  baseApiDio.options.connectTimeout = const Duration(seconds: 5);
+  baseApiDio.options.receiveTimeout = const Duration(seconds: 3);
+
+  // interceptors 추가
+  baseApiDio.interceptors.add(_CustomLogInterceptor());
+
+  AuthRepository(baseApiDio, baseUrl: baseUrl);
+}
+
+class _CustomLogInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    logger.d('REQUEST[${options.method}] => PATH: ${options.path}');
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    logger.d(
+      'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}',
+    );
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    logger.d(
+      'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
+    );
+    super.onError(err, handler);
+  }
 }
