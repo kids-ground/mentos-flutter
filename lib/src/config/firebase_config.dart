f43c3c3d6cb375ea44fc20df8469444c5e2dd601
RokwonK <wonrok97@naver.com>
2023-10-26 17:58:17 +0900

    ✨ notification config 추가

--- lib/src/config/firebase_config.dart
+++ lib/src/config/firebase_config.dart
@@ -1,24 +1,36 @@
 
-void setupFirebaseConfig() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:mentos_flutter/src/util/resource/logger.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'firebase_options.dart';
+
+// Notification 설정 제외
+Future<void> setupFirebaseConfig() async {
   // 공통설정
-  _setupCloudMessageConfig();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await _setupCloudMessaging();
   _setupCrashlyticsConfig();
-  _setupDynamicLinkConfig();
   _setupEventConfig();
 }
 
-// FCM 설정
-void _setupCloudMessageConfig() {
-
+// Firebase Cloud Message 설정
+Future<void> _setupCloudMessaging() async {
+  // iOS 앱이 포그라운드 상태에서도 알림 받을 수 있도록 설정
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: false,
+    sound: true,
+  );
 }
 
 // Firebase Crashlytics 설정
 void _setupCrashlyticsConfig() {
-
-}
-
-// Firebase DynamicLink 설정
-void _setupDynamicLinkConfig() {
 
 }
 
---
+++ lib/src/config/notification_config.dart
@@ -0,0 +1,59 @@
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:mentos_flutter/src/config/config.dart';
+import 'package:mentos_flutter/src/domain/service/DeepLinkingService.dart';
+import 'package:mentos_flutter/src/domain/service/NotificationService.dart';
+import 'package:mentos_flutter/src/util/resource/logger.dart';
+
+// Firebase 우선설정 필수
+Future<void> setupNotificationConfig(NotificationService notificationService, DeepLinkingService deepLinkingService) async {
+  await _setupRemoteNotificationHandler(deepLinkingService);
+  await notificationService.localNotificationConfig(
+      foregroundHandler: localNotificationHandlerFromForeground,
+      backgroundHandler: localNotificationHandlerFromBackground
+  );
+}
+
+Future<void> _setupRemoteNotificationHandler(DeepLinkingService deepLinkingService) async {
+  // Terminated 상태, Background, Foreground 에서 푸시 클릭
+  final receivedMessage = await FirebaseMessaging.instance.getInitialMessage();
+  remoteNotificationHandlerFromBackground(receivedMessage);
+  FirebaseMessaging.onMessageOpenedApp.listen(remoteNotificationHandlerFromBackground);
+  FirebaseMessaging.onMessage.listen(remoteNotificationHandlerFromForeground);
+}
+
+/*
+  모든 Notification Handler는 Entry Point(main)에서 접근가능하도록 전역으로 만들어야함
+*/
+Future<void> remoteNotificationHandlerFromBackground(RemoteMessage? message) async {
+  if (message != null && message.notification != null) {
+    getIt.get<DeepLinkingService>().saveDeepLink(remoteData: message.data);
+    logger.i('Remote Notification - Background : SUCCESS');
+  } else {
+    logger.e('Notification Error : 데이터가 들어 있지 않음');
+  }
+}
+
+Future<void> remoteNotificationHandlerFromForeground(RemoteMessage? message) async {
+  if (message != null && message.notification != null) {
+    getIt.get<DeepLinkingService>().saveDeepLink(remoteData: message.data);
+    logger.i('Remote Notification - Foreground : SUCCESS');
+  } else {
+    logger.e('Notification Error : 데이터가 들어 있지 않음');
+  }
+}
+
+Future<void> localNotificationHandlerFromForeground(NotificationResponse response) async {
+  if (response.payload != null) {
+    getIt.get<DeepLinkingService>().saveDeepLink(localData: response.payload);
+    logger.i('Local Notification - Foreground : SUCCESS');
+  }
+}
+
+Future<void> localNotificationHandlerFromBackground(NotificationResponse response) async {
+  if (response.payload != null) {
+    getIt.get<DeepLinkingService>().saveDeepLink(localData: response.payload);
+    logger.i('Local Notification - Foreground : SUCCESS');
+  }
+}
