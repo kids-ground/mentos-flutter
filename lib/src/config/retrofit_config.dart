578251935b769163408c9ab68c82a638ccbf1ff8
RokwonK <wonrok97@naver.com>
2023-11-05 19:25:43 +0900

    ✨ authorization interceptor 추가

--- lib/src/config/retrofit_config.dart
+++ lib/src/config/retrofit_config.dart
@@ -1,5 +1,7 @@
 
 import 'package:dio/dio.dart';
+import 'package:mentos_flutter/src/config/config.dart';
+import 'package:mentos_flutter/src/data/data_source/data_source.dart';
 import 'package:mentos_flutter/src/util/resource/logger.dart';
 
 late final Dio baseApiDio;
@@ -19,9 +21,35 @@ void _setupBaseApiDio() {
   baseApiDio.options.contentType = 'application/json';
 
   // interceptors 추가
+  baseApiDio.interceptors.add(_AuthorizationInterceptor());
   baseApiDio.interceptors.add(_CustomLogInterceptor());
 }
 
+// Authorization 관련
+class _AuthorizationInterceptor extends Interceptor {
+  String? accessToken;
+
+  @override
+  Future onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    if (accessToken == null) {
+      final storedAccessToken = await getIt.get<LocalKeyValueDataSource>().accessToken;
+      accessToken = storedAccessToken;
+    }
+
+    if (accessToken != null) options.headers['Authorization'] = 'Bearer ${accessToken!}';
+    super.onRequest(options, handler);
+  }
+
+  @override
+  Future onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401 || err.response?.statusCode == 403) {
+      // refresh
+    }
+    super.onError(err, handler);
+  }
+}
+
+// Log 관련
 class _CustomLogInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
